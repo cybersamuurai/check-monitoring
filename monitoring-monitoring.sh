8ed5da1420097e8d603be707fb8c980b17f5881a
Vladoss <swizzyde@gmail.com>
2023-11-02 16:30:21 +0300

    Add files via upload

---
+++ monitoring-monitoring.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true; do
+    # Здесь можно добавить команды для проверки мониторинга.
+
+    response=$(curl -s -o /dev/null -w "%{http_code}" https://monitoring.omp.ru/)
+
+    if [ "$response" -eq "200" ]; then
+        echo "Мониторинг работает нормально. Статус $response"
+    else
+        echo "Проблема с мониторингом! Статус $response"
+        # Здесь можно добавить дополнительные команды для уведомления о проблеме,
+    fi
+    
+    # Пауза между проверками
+    sleep 300
+done
